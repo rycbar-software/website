2837a04e888b81d018b9f6dcdf7e3152a4d21ad7
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-08-23 13:41:11 +0300

    Added link to dashboard

--- www/resources/views/layouts/navigation.blade.php
+++ www/resources/views/layouts/navigation.blade.php
@@ -51,7 +51,7 @@
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-
+                                <x-dropdown-link :href="route('dashboard')">Dashboard</x-dropdown-link>
                                 <x-dropdown-link :href="route('logout')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
