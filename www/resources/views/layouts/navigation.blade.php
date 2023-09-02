e5d46a4d2e97c8806052e19910e82f29c53e672c
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-02 11:09:46 +0300

    Fixed logout link

--- www/resources/views/layouts/navigation.blade.php
+++ www/resources/views/layouts/navigation.blade.php
@@ -90,24 +90,19 @@
                     <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
-            @endif
+                <div class="mt-3 space-y-1">
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
 
-            <div class="mt-3 space-y-1">
-{{--                <x-responsive-nav-link :href="route('profile.edit')">--}}
-{{--                    {{ __('Profile') }}--}}
-{{--                </x-responsive-nav-link>--}}
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        <x-responsive-nav-link :href="route('logout')"
+                                               onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            @endif
         </div>
     </div>
 </nav>
