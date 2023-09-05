0a48ed2f0ff5cd6206acb38383fb6ba6930989fa
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-05 13:56:52 +0300

    Fixed path to product store

--- www/resources/views/products/create.blade.php
+++ www/resources/views/products/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot:h1>Create product</x-slot:h1>
-    <x-forms.default-edit-form action="{{ route('articles.store') }}" :entity="null">
+    <x-forms.default-edit-form action="{{ route('products.store') }}" :entity="null">
         <x-forms.input-group>
             <x-forms.input-text name="external_link" title="External link:" value="" placeholder="External link"></x-forms.input-text>
         </x-forms.input-group>
