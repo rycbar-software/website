1e069fb19a3157602d2b6c81034e754e7782c688
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-16 13:14:11 +0300

    Added favicon

--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
             {!! seo() !!}
         @endif
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.ico">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="manifest" href="/site.webmanifest">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
@@ -90,7 +90,8 @@
               "name": "Rycbar Software",
               "serviceType": "Web development and maintaining",
               "description": "Development and long term maintenance of web applications",
-              "url": "https://{{ config('app.name') }}/"
+              "url": "https://{{ config('app.name') }}/",
+              "image": "https://{{ config('app.name') }}/favicon-32x32.ico"
             }
         </script>
     </body>
