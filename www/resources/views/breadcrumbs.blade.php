6e879b6b306a19b8f70509a2278bd212fdc0a986
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-28 11:05:50 +0300

    Fixed breadcrumbs markup

--- www/resources/views/breadcrumbs.blade.php
+++ www/resources/views/breadcrumbs.blade.php
@@ -2,7 +2,6 @@
     <nav class="container">
         <ol class="py-4 flex flex-wrap text-sm text-gray-800" itemscope itemtype="https://schema.org/BreadcrumbList">
             @foreach ($breadcrumbs as $key => $breadcrumb)
-
                 @if ($breadcrumb->url && !$loop->last)
                     <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
                         <a href="{{ $breadcrumb->url }}" itemprop="item" class="text-blue-600 hover:text-blue-900 hover:underline focus:text-blue-900 focus:underline">
@@ -12,7 +11,7 @@
                     </li>
                 @else
                     <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                        <span itemprop="item">
+                        <span>
                             <span itemprop="name">{{ $breadcrumb->title }}</span>
                             <meta itemprop="position" content="{{ $key + 1 }}" />
                         </span>
