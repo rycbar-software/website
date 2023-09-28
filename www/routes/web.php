01c0ffe32e5c7770466baa82e2fc051691ad35f5
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-28 11:54:45 +0300

    Fixed permissions issues

--- www/routes/web.php
+++ www/routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Enum\RoleEnum;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StaticController;
-use App\Http\Middleware\PublishedArticleMiddleware;
 use App\Models\Feedback;
 use Illuminate\Support\Facades\Route;
 
@@ -28,21 +27,23 @@ Route::get('/contacts', [StaticController::class, 'contacts'])->name('contacts')
 Route::get('/dashboard', [FeedbackController::class, 'index'])->middleware(['admin'])->name('dashboard');
 Route::post('/feedbacks/store', [FeedbackController::class, 'store'])->name('feedbacks.store');
 
-/*
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-*/
-
 require __DIR__.'/auth.php';
 
-Route::resource('articles', ArticleController::class);
-Route::resource('products', ProductController::class);
-Route::resource('partners', PartnerController::class);
+$resources = [
+    'articles' => ArticleController::class,
+    'products' => ProductController::class,
+    'partners' => PartnerController::class,
+];
+foreach ($resources as $resource => $controllerClass) {
+    Route::resource($resource, $controllerClass, [
+        'except' => ['index', 'show']
+    ])->middleware('role:' . RoleEnum::ADMIN->value);
+    Route::resource($resource, $controllerClass, [
+        'only' => ['index', 'show']
+    ]);
+}
 
 Route::delete('/feedbacks/{feedback}', function (Feedback $feedback) {
     $feedback->delete();
     return redirect()->route('dashboard');
-})->name('feedbacks.destroy');
+})->name('feedbacks.destroy')->middleware('role:' . RoleEnum::ADMIN->value);
