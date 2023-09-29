3ee8ae8fd486cb84b8011cb2c6c98609ad56461e
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-09-29 10:52:52 +0300

    Articles  markup

--- www/resources/views/articles/index.blade.php
+++ www/resources/views/articles/index.blade.php
@@ -7,28 +7,35 @@
     @can('create article')
         <x-forms.buttons.add href="{{ route('articles.create') }}"></x-forms.buttons.add>
     @endcan
-    <div class="flex">
-        <section itemscope itemtype="https://schema.org/ItemList">
+    <div class="articles-page">
+        <section itemscope itemtype="https://schema.org/ItemList" class="articles-page__list">
             @foreach($articles as $article)
                 <article class="article" itemprop="itemListElement" itemscope itemtype="https://schema.org/Article">
+                    <a href="{{ route('articles.show', $article) }}"><h2 class="article__name" itemprop="headline">{{ $article->getName() }}</h2></a>
                     <div class="article__info">
-                        <p class="article__Status">{{ $article->isPublished() ? 'Published' : 'Draft' }}</p>
                         <time datetime="{{ $article->created_at->toIso8601String() }}" class="article__date">{{ $article->publishDate() }}</time>
                         <meta itemprop="datePublished" content="{{ $article->created_at->toIso8601String() }}">
-                        <p class="article__author"><span>By </span><a class="link link--blue" href="https://a-kryvenko.com/" itemprop="author">Andriy Kryvenko</a></p>
+                        <meta itemprop="dateModified" content="{{ $article->updated_at->toIso8601String() }}">
+                        <p class="article__author">
+                            <span>&nbsp;by </span>
+                            <span itemprop="author" itemscope itemtype="https://schema.org/Person">
+                                <a class="link link--blue" itemprop="url" href="https://a-kryvenko.com/">
+                                    <span itemprop="name">Andriy Kryvenko</span>
+                                </a>
+                            </span>
+                        </p>
                     </div>
-                    <div class="article__content">
-                        <a href="{{ route('articles.show', $article) }}"><h2 class="article__name" itemprop="name">{{ $article->getName() }}</h2></a>
+                    <div class="article__tags">
                         <x-article-tag-list :article="$article"/>
-                        <div class="article__text content" itemprop="articleBody">{!! $article->preview_text !!}</div>
-                        <div class="article__details">
-                            <a href="{{ route('articles.show', $article) }}" class="link link--blue">Read more</a>
-                        </div>
+                    </div>
+                    <div class="article__text content" itemprop="articleBody">{!! $article->preview_text !!}</div>
+                    <div class="article__details">
+                        <a href="{{ route('articles.show', $article) }}" class="link link--blue">Read more</a>
                     </div>
                 </article>
             @endforeach
         </section>
-        <aside>
+        <aside class="articles-page__filter">
             <x-article-tag-filter/>
         </aside>
     </div>
